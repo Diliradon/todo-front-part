32ee39f49d714cf222ee09073764a813621078c2
Vladyslav_Zakharov <vladyslavzakharovdk@gmail.com>
2025-07-16 22:36:08 +0200

    Add Todo API and Type Definitions
    - Implemented CRUD operations for Todo items in the new api.ts file.
    - Defined Todo type in api.types.ts to structure Todo data.
    - Updated index.ts to export API functions and types for easier access.

---
+++ src/entities/todos/api.ts
@@ -0,0 +1,19 @@
+
+import { client } from '@/shared/lib/axios';
+import { Todo } from './api.types';
+
+export const getTodos = () => {
+  return client.get<Todo[]>('/todos');
+};
+
+export const createTodo = (todo: Todo) => {
+  return client.post('/todos', todo);
+};
+
+export const updateTodo = (todo: Todo) => {
+  return client.put(`/todos/${todo.id}`, todo);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
